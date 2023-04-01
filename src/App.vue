19960f8a420a569ffca36a2f8dc61800255d5833
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2023-04-01 19:01:47 +0100

    layout working

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <article>
     <div class="layout">
-      <header class="layout__header header">
-        <div class="brand">
-          <h1 class="brand__sig">Clarky</h1>
-          <p class="brand__strap">Technology / Music</p>
-        </div>
-      </header>
-      <section class="layout__page">        
-        <menu class="layout__menu menu">
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </menu>   
-        <div class="layout__content content"> 
-          <router-view/>
-        </div>
-      </section>
-    </div>
-    <footer class="layout__footer footer">
+      <div class="layout__top">
+        <header class="layout__header header">
+          <div class="brand">
+            <h1 class="brand__sig">Clarky</h1>
+            <p class="brand__strap">Technology / Music</p>
+          </div>
+        </header>
+        <section class="layout__page">        
+          <menu class="layout__menu menu">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+          </menu>   
+          <div class="layout__content content"> 
+            <router-view/>
+          </div>
+        </section>
+      </div>
+      <footer class="layout__footer footer">
         <p>Made in Bristol, UK with Vue, Express and Heroku</p>
         <p><a href="https://github.com/netotaku/clarky2" target="_blank">View on GitHub</a></p>
-    </footer>
+      </footer>
+    </div>
+
   </article>
 </template>
 
@@ -47,25 +50,57 @@
   }
   
   .layout{
-    
-    background: #fff;
+
+    @media (min-width:600px)  { 
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+    }   
+
+    &__top{
+      @media (min-width:600px)  { 
+        display: flex;
+        flex-grow: 1;
+      }         
+    }
 
     &__menu{
       padding: $gutter;
+      @media (min-width:600px)  { 
+        
+      } 
     }
 
     &__header{
+      position: relative;
       padding: $gutter;
-    } 
-    &__content{
+      height: 80vh;
+      @media (min-width:600px)  { 
+        height: auto;
+        width: 420px;
+      } 
+    }
+
+    &__content{      
       padding: $gutter;
       @media (min-width:600px)  { 
-          display: flex;
+        display: flex;
       }      
     }
+
+    &__page{
+      flex-grow: 1;
+      background: #fff;
+      @media (min-width:600px)  { 
+        
+      }     
+      
+    }
+
     &__footer{
       padding: $gutter;
-    }   
+    }
+
   }
 
   .header{
@@ -76,6 +111,9 @@
     background-position: center top;
     background-attachment: fixed;
     background-size: cover;
+    @media (min-width:600px)  { 
+        background-attachment: scroll;
+    } 
   }
 
   .content{
@@ -87,7 +125,18 @@
   }
 
   .brand{
-
+    position: absolute;
+    left: $gutter;
+    right: $gutter;
+    bottom: $gutter;
+    &__sig{
+      border-bottom: solid 3px white;
+      padding-bottom: 3px;
+      margin-bottom: 3px;
+    }
+    @media (min-width:600px)  { 
+        position: static;
+    } 
   }
   
   .footer{
