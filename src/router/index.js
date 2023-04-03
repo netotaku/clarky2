ea48fd126e56c5b2b1c9c3a8a4183bd9f9e71f04
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2023-04-03 09:48:10 +0100

    404

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Page from '../views/Page.vue'
+import four0four from '../views/404.vue'
 import sitedata from "../json/site.json"
 
 let routes = [];
@@ -11,6 +12,12 @@ for(let r in sitedata){
     component: Page
   })
 }
+
+routes.push({
+  path: '/:pathMatch(.*)*',
+  name: "404",
+  component: four0four
+})
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
---
+++ src/views/404.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1 class="spotlight__h2">404</h1>
+    <p>Sorry, something has gone wrong, this page has moved or does not exist. <a href="/">Return home</a></p>      
+</template>
