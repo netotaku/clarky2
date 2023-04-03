5f680cc50659475270d7c8b93830d9a5d780d977
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2023-04-03 09:56:05 +0100

    404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,4 +1,6 @@
 <template>
-    <h1 class="spotlight__h2">404</h1>
-    <p>Sorry, something has gone wrong, this page has moved or does not exist. <a href="/">Return home</a></p>      
+    <section class="hg__u hg__u--ful">
+        <h1 class="spotlight__h2">404</h1>
+        <p>Sorry, something has gone wrong, this page has moved or does not exist. <router-link to="/">Return home</router-link></p>      
+    </section>
 </template>
